4cb3f35f1518a13aa1dbe50c266f1775b24c766d
NAR <yyazok@gmail.com>
2025-03-12 23:08:44 +0300

    v1.2

--- script.js
+++ script.js
@@ -18,56 +18,24 @@ document.addEventListener("DOMContentLoaded", () => {
     let repeat = false;
 
     const music = [
-        { title: "Nerdeyim", album: "1209", source: "https://cdn1.suno.ai/6aa47c7a-3fe2-4c00-89d4-f3395b74d24f.mp3", cover: "https://cdn2.suno.ai/image_6aa47c7a-3fe2-4c00-89d4-f3395b74d24f.jpeg" },
-        { title: "Ilham", album: "1209", source: "https://cdn1.suno.ai/2b69a730-9375-42f7-a349-c4e494483200.mp3", cover: "https://cdn2.suno.ai/image_2b69a730-9375-42f7-a349-c4e494483200.jpeg" },
-        { title: "Illegal takılıyoruz",  album: "1209", source: "https://cdn1.suno.ai/5b8ef581-6354-427b-8e95-523306ed60de.mp3", cover: "https://cdn2.suno.ai/image_5b8ef581-6354-427b-8e95-523306ed60de.jpeg" },
-        { title: "Je tourne",  album: "1709", source: "https://cdn1.suno.ai/e4676b9e-ef2b-4053-be63-ffd5b09fca2b.mp3", cover: "https://cdn2.suno.ai/image_e4676b9e-ef2b-4053-be63-ffd5b09fca2b.jpeg" },
-        { title: "Kalp",  album: "1209", source: "https://cdn1.suno.ai/90cc2b38-614a-437d-b064-68c514510b3b.mp3", cover: "https://cdn2.suno.ai/image_90cc2b38-614a-437d-b064-68c514510b3b.jpeg" },
-        { title: "Olmuş",  album: "1709", source: "https://cdn1.suno.ai/6d811389-b8a3-423f-abf2-4742812639ff.mp3", cover: "https://cdn2.suno.ai/image_6d811389-b8a3-423f-abf2-4742812639ff.jpeg" },
-        { title: "Sor",  album: "1709", source: "https://cdn1.suno.ai/317c2d40-2a5b-47e3-bd51-ef3e0c94d594.mp3", cover: "https://cdn2.suno.ai/image_317c2d40-2a5b-47e3-bd51-ef3e0c94d594.jpeg" },
-        { title: "Karar",  album: "1209", source: "https://cdn1.suno.ai/c3ca2154-f8bb-4fa5-acb6-0266e2e2eee1.mp3", cover: "https://cdn2.suno.ai/image_c3ca2154-f8bb-4fa5-acb6-0266e2e2eee1.jpeg" },
-        { title: "Karamsar",  album: "1209", source: "https://cdn1.suno.ai/b6207bc5-f700-4947-932d-5d9050a9ebc8.mp3", cover: "https://cdn2.suno.ai/image_b6207bc5-f700-4947-932d-5d9050a9ebc8.jpeg" },
-        { title: "Karga",  album: "1709", source: "https://cdn1.suno.ai/82c117d8-49b2-4a8a-84d2-29ecdda3582f.mp3", cover: "https://cdn2.suno.ai/82c117d8-49b2-4a8a-84d2-29ecdda3582f_d8f71e5a.jpeg" },
-        { title: "Halüsinasyon",  album: "1209", source: "https://cdn1.suno.ai/1f55ba94-38c5-4a69-aa07-278330314df4.mp3", cover: "https://cdn2.suno.ai/image_1f55ba94-38c5-4a69-aa07-278330314df4.jpeg" },
-        { title: "Ben",  album: "1709", source: "https://cdn1.suno.ai/cbfdcd6c-8dea-434d-8dc1-49112fdbd03d.mp3", cover: "https://cdn2.suno.ai/image_cbfdcd6c-8dea-434d-8dc1-49112fdbd03d.jpeg" }
-    ];
+        { title: "Nerdeyim", album: "1209" },
+        { title: "Ilham", album: "1209" },
+        { title: "Illegal takılıyoruz", album: "1209" },
+        { title: "Je tourne", album: "304" },
+        { title: "Kalp", album: "1209" },
+        { title: "Olmuş", album: "1709" },
+        { title: "Sor", album: "1709" },
+        { title: "Karar", album: "1209" },
+        { title: "Karamsar", album: "1209" },
+        { title: "Karga", album: "1709" },
+        { title: "Ben", album: "1709" }
+    ];    
     
     const foundMusic = music.findIndex(song => song.title === musicTitle);
     if (foundMusic >= 0) {
         currentTrackIndex = foundMusic;
     } else {
         currentTrackIndex = 0;
-    }
-
-    function getDominantColor(imageUrl, callback) {
-        const img = new Image();
-        img.crossOrigin = "Anonymous";
-        img.src = imageUrl;
-        img.onload = function () {
-            const canvas = document.createElement("canvas");
-            canvas.width = this.width;
-            canvas.height = this.height;
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(this, 0, 0);
-
-            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            const data = imageData.data;
-            let r = 0,
-                g = 0,
-                b = 0;
-
-            for (let i = 0; i < data.length; i += 4) {
-                r += data[i];
-                g += data[i + 1];
-                b += data[i + 2];
-            }
-
-            r = Math.floor(r / (data.length / 4));
-            g = Math.floor(g / (data.length / 4));
-            b = Math.floor(b / (data.length / 4));
-
-            callback(`rgb(${r},${g},${b})`);
-        };
     }
 
     function toggleMenu() {
@@ -84,9 +52,6 @@ document.addEventListener("DOMContentLoaded", () => {
               <b>${station.title}</b>
               <i>${station.album}</i>
           `;
-            getDominantColor(station.cover, (color) => {
-                stationElement.style.backgroundColor = color;
-            });
             stationElement.addEventListener("click", () => {
                 currentTrackIndex = index;
                 playTrack();
@@ -100,18 +65,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const currentTrack = music[currentTrackIndex];
         musicName.textContent = currentTrack.title;
         artistName.textContent = currentTrack.album;
-        backgroundOverlay.style.backgroundImage = `url('${currentTrack.cover}')`;
+        backgroundOverlay.style.backgroundImage = `url('https://devnar.github.io/radio/cover/${currentTrack.title.toLowerCase()}.png')`;
     }
 
     function playTrack() {
         const currentTrack = music[currentTrackIndex];
-        audioPlayer.src = currentTrack.source;
+        audioPlayer.src = `https://devnar.github.io/radio/music/${currentTrack.title.toLowerCase()}.mp3`;
         audioPlayer.play();
         updateTrackInfo();
         playPauseButton.innerHTML = '<i data-lucide="pause"></i>';
-        getDominantColor(currentTrack.cover, (color) => {
-            document.documentElement.style.setProperty("--theme-color", color);
-        });
         lucide.createIcons();
     }
 
